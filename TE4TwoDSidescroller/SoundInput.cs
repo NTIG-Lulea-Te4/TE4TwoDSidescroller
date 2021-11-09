095a26b89b3a5fefd88e4b1afb313900a4281276
Queen <ruben.bergenstrale@elev.ga.ntig.se>
2021-11-09 15:26:23 +0100

    Created Animation and Sound classes

---
+++ TE4TwoDSidescroller/SoundInput.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TE4TwoDSidescroller
+{
+    class SoundInput
+    {
+
+        public void PlaySound()
+        {
+            SoundEffect effect;
+
+            effect = Content.Load<SoundEffect>("effect");
+        }
+
+
+
+
+    }
+}
