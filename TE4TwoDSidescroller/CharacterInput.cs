6869d19ca04084910c1faa8923be8defbd81907c
abbe_F <fazeli308@gmail.com>
2021-11-09 08:53:26 +0100

    Added a very basic movement

--- TE4TwoDSidescroller/CharacterInput.cs
+++ TE4TwoDSidescroller/CharacterInput.cs
@@ -1,11 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace TE4TwoDSidescroller
 {
     class CharacterInput
     {
 
+        private Vector2 position;
+
+        protected virtual void MovementInput()
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                position.Y -= 1;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                position.Y += 1;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                position.X -= 1;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                position.X += 1;
+            }
+        }
     }
 }
