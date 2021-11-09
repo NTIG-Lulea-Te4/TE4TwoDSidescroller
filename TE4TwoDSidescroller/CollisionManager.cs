d71cda92fecd6b9d9c5a937ab2fa6cafec34ac95
Izze <william.isacsson@elev.ga.ntig.se>
2021-11-09 08:54:15 +0100

    Skapat en CollisionManager klass
    I klassen finns en metod som tar in två rektanglar och ger tillbaks true ifall dem har kolliderat

---
+++ TE4TwoDSidescroller/CollisionManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace TE4TwoDSidescroller
+{
+    class CollisionManager
+    {
+        public static bool CollisionChecker(Rectangle firstTargetToCheck, Rectangle secondTargetToCheck)
+        {
+
+            if (firstTargetToCheck.Width + firstTargetToCheck.X >= secondTargetToCheck.X
+                && firstTargetToCheck.Height + firstTargetToCheck.Y >= secondTargetToCheck.Y
+                && firstTargetToCheck.X < secondTargetToCheck.X + secondTargetToCheck.Width
+                && firstTargetToCheck.Y < secondTargetToCheck.Y + secondTargetToCheck.Height)
+            {
+
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
