15178d41c22545c4198f4bc9e6850bac39cc3ba4
abbe_F <fazeli308@gmail.com>
2021-11-10 15:05:46 +0100

    Replaced Keys in different class
    Added Keys & Update to PlayInput
    
    Removed Movement from CharacterInput
    
    Added Movement to Character

--- TE4TwoDSidescroller/Character.cs
+++ TE4TwoDSidescroller/Character.cs
@@ -1,19 +1,116 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace TE4TwoDSidescroller
 {
     class Character : Entity
     {
-        protected bool ifHit;
-        protected int health;
-        protected int speed;
-        Character()
+        CharacterInput characterInput;
+
+        private Vector2 characterPosition;
+
+        private float walkSpeed;
+        private float runSpeed;
+
+        private float characterVelocity;
+
+        private float characterJumpHeight;
+
+        private bool ifHit;
+        private int health;
+        private int speed;
+        public Character()
+        {
+            characterInput = new CharacterInput(this);
+        }
+
+        #region Movement
+        public virtual void GoesUp()
         {
             
         }
 
+        public virtual void GoesDown()
+        {
+            
+        }
+
+        public virtual void GoesLeft()
+        {
+            
+        }
+
+        public virtual void GoesRight()
+        {
+            
+        }
+
+        public virtual void Run()
+        {
+            
+        }
+
+        public virtual void Jump()
+        {
+
+        }
+
+        public virtual void DoubleJump()
+        {
+
+        }
+
+        public virtual void Crouch()
+        {
+
+        }
+
+        public virtual void Dash()
+        {
+
+        }
+        #endregion
+
+        #region Attacks
+
+        public virtual void LightAttack()
+        {
+
+        }
+
+        public virtual void HeavyAttack()
+        {
+
+        }
+
+        public virtual void SpecialAttack()
+        {
+
+        }
+        #endregion
+
+        #region Conditions
+
+        public virtual void Parry()
+        {
+
+        }
+
+        public virtual void Block()
+        {
+
+        }
+
+        public virtual void Dodge()
+        {
+
+        }
+
+        #endregion
         protected void Health()
         {
 
