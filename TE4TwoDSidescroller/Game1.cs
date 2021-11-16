3d1e11a73ce223a322bea6e3ff981120729ae572
MnHed <mans.hedlund@elev.ga.ntig.se>
2021-11-16 09:34:03 +0100

    2021-11-16
    creationmanager

--- TE4TwoDSidescroller/Game1.cs
+++ TE4TwoDSidescroller/Game1.cs
@@ -7,30 +7,29 @@ namespace TE4TwoDSidescroller
 {
     public class Game1 : Game
     {
-        public static GraphicsDeviceManager graphicsDeviceManager;
-        public Texture2D rightWalk;
-        Rectangle destinationRectangle;
-        Rectangle sourceRectangle;
-            Floor floorTest;
+        
+            
 
         public Game1()
         {
-            floorTest = new Floor();
+            GameInfo.creationManager = new CreationManager();
             GameInfo.graphicsDevice = new GraphicsDeviceManager(this);
             GameInfo.collisionManager = new CollisionManager();
             GameInfo.entityManager = new EntityManagear();
             GameInfo.collisionManager = new CollisionManager();
+            GameInfo.screenManager = new ScreenManager();
+
+            
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
-            destinationRectangle = new Rectangle(100, 100, 32, 46);
             // TODO: Add your initialization logic here
 
 
-            floorTest.Initialize();
+            
             base.Initialize();
         }
 
@@ -38,15 +37,8 @@ namespace TE4TwoDSidescroller
         {
             GameInfo.spriteBatch = new SpriteBatch(GameInfo.graphicsDevice.GraphicsDevice);
 
-            string currentPath = Path.GetDirectoryName(
-          System.Reflection.Assembly.GetExecutingAssembly().Location)
-          + "/Content/Pngs/" + "ShadowRunRight.png";
-            using (Stream textureStream = new FileStream(currentPath, FileMode.Open))
-            {
-                rightWalk = Texture2D.FromStream(GraphicsDevice, textureStream);
-            }
-
             // TODO: use this.Content to load your game content here
+            
         }
 
         protected override void Update(GameTime gameTime)
@@ -54,7 +46,7 @@ namespace TE4TwoDSidescroller
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            sourceRectangle = new Rectangle(0, 0, 32, 46);
+            
             
             // TODO: Add your update logic here
 
@@ -64,15 +56,14 @@ namespace TE4TwoDSidescroller
         protected override void Draw(GameTime gameTime)
         {
             GameInfo.graphicsDevice.GraphicsDevice.Clear(Color.CornflowerBlue);
-            GameInfo.entityManager.AddEntity(floorTest);
+            
 
             GameInfo.spriteBatch.Begin();
 
-            //GameInfo.entityManager.Draw(gameTime);
-
-            floorTest.Draw(gameTime);
             
-            GameInfo.spriteBatch.Draw(rightWalk, destinationRectangle, sourceRectangle, Color.White);
+            GameInfo.entityManager.Draw(gameTime);
+            
+            
             GameInfo.spriteBatch.End();
 
             base.Draw(gameTime);
