bad536f6ca42dffd5306b5de634b7022f1e35106
MnHed <mans.hedlund@elev.ga.ntig.se>
2021-11-22 15:19:44 +0100

    2021-11-22
    farmer

--- TE4TwoDSidescroller/Farmer.cs
+++ TE4TwoDSidescroller/Farmer.cs
@@ -12,16 +12,22 @@ namespace TE4TwoDSidescroller
 {
     class Farmer : Character
     {
-        
+
         int currentHealth;
         int manaCheck;
         int maxHealth;
         int manaTick;
         int mana;
+        int movementSpeed;
+        int jumpHeight;
 
         Texture2D myTexture;
         Vector2 myPosition;
         Rectangle myRectangle;
+        Rectangle sourceRectangle;
+        int frames;
+        
+
 
         public Farmer()
         {
@@ -30,7 +36,96 @@ namespace TE4TwoDSidescroller
             mana = 100;
             manaCheck = mana;
             manaTick = 0;
+            movementSpeed = 5;
+            jumpHeight = 3;
+            frames = 0;
+
+            myRectangle = new Rectangle((int)myPosition.X, (int)myPosition.Y, myTexture.Width, myTexture.Height);
+            sourceRectangle = new Rectangle(32 * frames, 0, 32, 48);
+            string currentPath = 
+            Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)+ "/Farmer.png";
+
+            using (Stream textureStream = new FileStream(currentPath, FileMode.Open))
+            {
+
+                myTexture = Texture2D.FromStream(GameInfo.graphicsDevice.GraphicsDevice, textureStream);
+
+            }
+
+            
         }
+
+
+        #region Actions
+
+        public override void MoveRight()
+        {
+
+            myPosition.X = myPosition.X + movementSpeed;
+
+        }
+
+        public override void MoveLeft()
+        {
+
+            myPosition.X = myPosition.X - movementSpeed;
+
+        }
+
+        public override void Jump()
+        {
+
+            myPosition.Y = myPosition.Y + jumpHeight;
+
+        }
+
+        public override void Attack1()
+        {
+
+
+
+        }
+
+
+
+        #endregion
+
+
+        #region Texture/Animation
+
+        void GetMyFrame(int frameNumber)
+        {
+
+            if (frameNumber == 0)
+            {
+                frames = frameNumber;
+
+            }
+
+            if (frameNumber == 1)
+            {
+                frames = frameNumber;
+
+            }
+            
+            if (frameNumber == 2)
+            {
+                frames = frameNumber;
+
+            }
+            
+            if (frameNumber == 3)
+            {
+                frames = frameNumber;
+
+            }
+
+
+        }
+
+
+        #endregion
+
 
         public override void Update(GameTime gameTime)
         {
@@ -40,15 +135,17 @@ namespace TE4TwoDSidescroller
                 mana++;
                 manaTick = 0;
             }
-        
-            
-            
-        }   
+
+
+
+
+
+        }
 
         public override void Draw(GameTime gameTime)
         {
-        
-            GameInfo.spriteBatch.Draw();
+
+            GameInfo.spriteBatch.Draw(myTexture, myRectangle, sourceRectangle , Color.White);
 
         }
 
