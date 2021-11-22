2c38910c4a60c078f4564c4d2206ab18ef085b59
MnHed <mans.hedlund@elev.ga.ntig.se>
2021-11-22 15:20:22 +0100

    2021-11-22 final

--- TE4TwoDSidescroller/FarmerInput.cs
+++ TE4TwoDSidescroller/FarmerInput.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
 using System.Text;
 
 namespace TE4TwoDSidescroller
 {
-    class FarmerInput
+    class FarmerInput : CharacterInput
     {
+
+        public FarmerInput(Character character) : base (character)
+        {  
+
+
+
+        }
+    
+    
+        public override void Update(GameTime gameTime)
+        {
+
+
+
+        }
+    
     }
 }
